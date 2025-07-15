7b911aad306f541034c8acd94c969979df168a0c
wkdan <jmy8876@gmail.com>
2025-07-15 21:52:18 +0900

    ✨ feat: ReviewRepository 수정

--- src/main/java/com/grow/member_service/review/domain/repository/ReviewRepository.java
+++ src/main/java/com/grow/member_service/review/domain/repository/ReviewRepository.java
@@ -1,10 +1,12 @@
 package com.grow.member_service.review.domain.repository;
 
-import com.grow.member_service.review.domain.model.Review;
-
 import java.util.List;
+import java.util.Optional;
+
+import com.grow.member_service.review.domain.model.Review;
 
 public interface ReviewRepository {
     Review save(Review review);
     List<Review> findByRevieweeId(Long revieweeId);
+    Optional<Review> findByReviewerIdAndRevieweeId(Long reviewerId, Long revieweeId);
 }
