a8feab8a64aad7dfe9b02a29b7c6a6d7439721d3
wkdan <jmy8876@gmail.com>
2025-07-15 21:09:20 +0900

    ✨ feat: 리뷰 클래스에 점수 필드 추가 및 평균 점수 계산 기능 구현

--- src/main/java/com/grow/member_service/review/domain/model/Review.java
+++ src/main/java/com/grow/member_service/review/domain/model/Review.java
@@ -4,35 +4,36 @@ import lombok.Getter;
 
 @Getter
 public class Review {
-
     private final Long reviewId;
-    private final Long reviewerId; // 리뷰 작성자 ID
-    private final Long revieweeId; // 리뷰 대상자 ID
-    private final String content; // 평가 내용
-    private final Double totalScore; // 총 점수
+    private final Long reviewerId;
+    private final Long revieweeId;
+    private final String content;
 
-    public Review(Long reviewerId,
-                  Long revieweeId,
-                  String content,
-                  Double totalScore
-    ) {
-        this.reviewId = null;
-        this.reviewerId = reviewerId;
-        this.revieweeId = revieweeId;
-        this.content = content;
-        this.totalScore = totalScore;
+    private final int sincerityScore;
+    private final int enthusiasmScore;
+    private final int communicationScore;
+    private final Double totalScore;
+
+    public Review(Long reviewerId, Long revieweeId, String content,
+        int sincerityScore, int enthusiasmScore, int communicationScore) {
+        this(null, reviewerId, revieweeId, content,
+            sincerityScore, enthusiasmScore, communicationScore);
     }
 
-    public Review(Long reviewId,
-                  Long reviewerId,
-                  Long revieweeId,
-                  String content,
-                  Double totalScore
-    ) {
+    public Review(Long reviewId, Long reviewerId, Long revieweeId, String content,
+        int sincerityScore, int enthusiasmScore, int communicationScore) {
         this.reviewId = reviewId;
         this.reviewerId = reviewerId;
         this.revieweeId = revieweeId;
         this.content = content;
-        this.totalScore = totalScore;
+        this.sincerityScore = sincerityScore;
+        this.enthusiasmScore = enthusiasmScore;
+        this.communicationScore = communicationScore;
+        this.totalScore = calculateAverageScore();
+    }
+
+    /** 리뷰 점수의 평균을 계산합니다. */
+    private double calculateAverageScore() {
+        return (sincerityScore + enthusiasmScore + communicationScore) / 3.0;
     }
 }
