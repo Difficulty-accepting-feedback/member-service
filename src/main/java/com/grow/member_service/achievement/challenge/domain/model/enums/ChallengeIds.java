c0b340fbd9fb7a1857d8bb37686cc1165c21c47b
wkdan <jmy8876@gmail.com>
2025-08-26 22:47:31 +0900

    ✨ feat: 챌린지 ID 상수 및 초기 데이터 삽입 추가

---
+++ src/main/java/com/grow/member_service/achievement/challenge/domain/model/enums/ChallengeIds.java
@@ -0,0 +1,14 @@
+package com.grow.member_service.achievement.challenge.domain.model.enums;
+
+/**
+ * 챌린지 ID 상수 모음
+ */
+public final class ChallengeIds {
+	private ChallengeIds() {}
+
+	public static final long FIRST_LOGIN        = 1001L;
+	public static final long PHONE_VERIFIED     = 1003L;
+	public static final long ADDRESS_SET        = 1004L;
+	public static final long FIRST_PAYMENT      = 4001L;
+	public static final long FIRST_SUBSCRIPTION = 4002L;
+}
