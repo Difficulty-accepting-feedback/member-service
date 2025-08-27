adf79f9d13805e86d5fa84cde4a5f8d2cca2a10d
wkdan <jmy8876@gmail.com>
2025-08-27 17:47:39 +0900

    ✨ feat: 비동기 처리 설정 추가

---
+++ src/main/java/com/grow/member_service/global/config/AsyncConfig.java
@@ -0,0 +1,8 @@
+package com.grow.member_service.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig { }
