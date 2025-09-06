5c6d111f8404aed85edad7558adca049af165f89
wkdan <jmy8876@gmail.com>
2025-09-06 21:51:40 +0900

    ✨ feat: JsonUtils.toJsonString 메서드의 제네릭 타입 파라미터 추가

--- src/main/java/com/grow/member_service/global/util/JsonUtils.java
+++ src/main/java/com/grow/member_service/global/util/JsonUtils.java
@@ -22,11 +22,11 @@ public class JsonUtils {
 	 * 이 메서드는 Jackson의 ObjectMapper를 사용하여 객체를 직렬화합니다.
 	 * 변환 중 오류가 발생하면 RuntimeException을 던집니다.
 	 *
-	 * @param object JSON으로 변환할 객체. null이 허용되며, null은 "null" 문자열로 변환됩니다.
+	 * @param <T> JSON으로 변환할 객체. null이 허용되며, null은 "null" 문자열로 변환됩니다.
 	 * @return 객체를 나타내는 JSON 문자열.
 	 * @throws RuntimeException JSON 직렬화에 실패할 경우 발생합니다. 내부적으로 JsonProcessingException을 래핑합니다.
 	 */
-	public static String toJsonString(Object object) {
+	public static <T> String toJsonString(T object) {
 		try {
 			return objectMapper.writeValueAsString(object);
 		} catch (JsonProcessingException e) {
